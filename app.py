627c0fce593b5cad7ecd93ea2b8abd22b666530f
Administrator <842864689@qq.com>
2017-08-01 20:36:50 +0800

    亮度越大，频率越大

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template, Response, send_from_directory, send_file
+from flask import Flask, render_template, Response, send_from_directory, send_file, g, request
 from camera import Camera
-import time,threading
+import time, threading
+from PIL import Image, ImageStat
+from types import MethodType
 
 app = Flask(__name__)
 SLEEP_TIME_LONG = 0.1
+
+ff = 3
+
+
+def brightness(im_file):
+    # im = Image.open(im_file).convert('L')
+    stat = ImageStat.Stat(im_file)
+    mm = stat._getextrema()
+    return stat.mean[0]
+
+
+@app.before_request
+def before_request():
+    global ff
 
 
 @app.route('/')
@@ -11,32 +27,71 @@ def index():
     return render_template('index.html')
 
 
+def sleep_s(f):
+    global cnt
+    while True:
+        # with app.app_context():
+        #     g.cnt += 1
+        cnt += 1
+        time.sleep(1 / f)
 
 
 def gen(camera, f):
-    threading.Thread
+    global ff
+    global cnt
+    cnt = 0
+    tmpcnt = 0
+
+    frame = camera.get_frame()
+    flag = 1
+    last = time.time()
+    frame1 = open('test6.jpg', 'rb').read()
+    im = Image.open('test1.jpg').convert('L')
+    stat = ImageStat.Stat(im)
+    mm = stat._getextrema()
+    print(mm)
+    # print(frame)
     while True:
-        if f != 0:
-            temp=(b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            time.sleep(1 / f)
-        frame = camera.get_frame()
-        time.sleep(SLEEP_TIME_LONG)
-        # print('*************')
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        f = ff
+        a = time.time()
+        interval = 0.1
+        # print(f)
+        if interval > a - last > 0:
+            # print(a, last, 111111111111)
+            temp = b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame1 + b'\r\n'
+            yield temp
+        elif interval + f > a - last >= interval:
+            # print(a, last, 222222222222)
+            frame, grey = camera.get_frame()
+            ngrey = brightness(grey)
+
+            # print(ngrey)
+            ff = (ngrey+10) / 100
+            yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+        elif a - last >= interval + f:
+            last = time.time()
+            # print(a, last, 333333333333)
 
 
-@app.route('/video_feed/f/<int:f>')
+@app.route('/vid/eo_feed/f/<float:f>')
 def video_f(f):
     return Response(gen(Camera(), f),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route("/process_data")
+def process_data():
+    global ff
+    f = float(request.args.get('f', 10)) / 5
+    ff = f
+    print(ff)
+    return "200 OK"
+
+
 @app.route('/video_feed')
 def video_feed():
     # t = threading.Thread(target=gen(), name='LoopThread')
-    return Response(gen(Camera(),0),
+    return Response(gen(Camera(), 10),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
@@ -46,4 +101,5 @@ def get_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    cnt = 0
+    app.run(host='0.0.0.0', threaded=True)
--- camera.py
+++ camera.py
@@ -1,6 +1,8 @@
 # camera.py
 
 from VideoCapture import Device
+from PIL import Image
+from win32api import GetSystemMetrics
 import time
 import sys, pygame
 
@@ -16,6 +18,7 @@ import sys, pygame
 # 抓取频率，抓取一次
 SLEEP_TIME_LONG = 0.1
 
+
 # 初始化摄像头
 
 
@@ -26,27 +29,37 @@ class Camera(object):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
     cam = Device(devnum=0, showVideoWindow=0)
+
     def __init__(self):
         # self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
         pass
 
     def get_frame(self):
-        self.cam.saveSnapshot('test.jpg', timestamp=3, boldfont=1, quality=75)
-        a=open('test.jpg', 'rb').read()
-        return a
+        width = GetSystemMetrics(0)
+        nwidth = int(width * 0.9)
+        height = GetSystemMetrics(1)
+        nheight = int(height * 0.9)
+        mi=min(width,height)
+        self.cam.saveSnapshot('test_raw.jpg', timestamp=3, boldfont=1, quality=75)
+        im = Image.open("test_raw.jpg")
+        out = im.resize((mi, mi))
+        out.save("test.jpg")
+        a = open('test.jpg', 'rb').read()
+        b = Image.open('test.jpg', 'r').convert('L')
+        return a, b
 
-# while True:
-#     # 抓图
-#     cam.saveSnapshot('test.jpg', timestamp=3, boldfont=1, quality=75)
-#
-#     # 加载图像
-#     image = pygame.image.load('test.jpg')
-#
-#     # 传送画面
-#     screen.blit(image, speed)
-#
-#     # 显示图像
-#     pygame.display.flip()
-#     # 休眠一下，等待一分钟
-#     time.sleep(SLEEP_TIME_LONG)
+        # while True:
+        #     # 抓图
+        #     cam.saveSnapshot('test.jpg', timestamp=3, boldfont=1, quality=75)
+        #
+        #     # 加载图像
+        #     image = pygame.image.load('test.jpg')
+        #
+        #     # 传送画面
+        #     screen.blit(image, speed)
+        #
+        #     # 显示图像
+        #     pygame.display.flip()
+        #     # 休眠一下，等待一分钟
+        #     time.sleep(SLEEP_TIME_LONG)
